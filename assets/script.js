efa76d7e3312ba654b0259fc0cad246f618d874d
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-01-11 14:11:56 -0500

    conflict fix maybe

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=mXyNr2iOtjbdhpk5dAR4WjUVoO6JQW2d"
-=======
 $(".btn-secondary").on("click", function() {
 
 var SearchTerm = $("search-term-form").val()
@@ -9,7 +6,6 @@ var year = 1990;
 
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + SearchTerm + "&page=" + numResults + "&pub_year=" + year +"&api-key=mXyNr2iOtjbdhpk5dAR4WjUVoO6JQW2d"
->>>>>>> 46c20a52dfdfac8c2b65ec1c4a4b38f150ba6a0c
 
 
 
@@ -18,13 +14,7 @@ $.ajax({
         method: "GET"
     })
     .then(function(response) {
-<<<<<<< HEAD
-
-
-        }
-=======
 console.log(response);
     
     });
 });
->>>>>>> 46c20a52dfdfac8c2b65ec1c4a4b38f150ba6a0c
