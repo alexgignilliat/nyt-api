6bc0e1a60f3774ac59299122805c9f3ecf4b4474
Rojel Perez Vez <rojelpv@gmail.com>
2020-01-11 13:35:52 -0500

    footer

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,12 @@
-
-
-
-
-
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=mXyNr2iOtjbdhpk5dAR4WjUVoO6JQW2d"
 
 
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
+        url: queryURL,
+        method: "GET"
+    })
     .then(function(response) {
 
 
-    }
+        }
