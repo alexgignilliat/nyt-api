4505e5067714ba10d9430072a7e8e3bfe0dbef95
Rojel Perez Vez <rojelpv@gmail.com>
2020-01-14 18:07:05 -0500

    footer

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,20 @@
-<<<<<<< HEAD
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=mXyNr2iOtjbdhpk5dAR4WjUVoO6JQW2d"
-=======
 $(".btn-secondary").on("click", function() {
 
-var SearchTerm = $("search-term-form").val()
-var numResults = 5;
-var year = 1990;
+    var SearchTerm = $("search-term-form").val()
+    var numResults = 5;
+    var year = 1990;
 
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + SearchTerm + "&page=" + numResults + "&pub_year=" + year +"&api-key=mXyNr2iOtjbdhpk5dAR4WjUVoO6JQW2d"
->>>>>>> 46c20a52dfdfac8c2b65ec1c4a4b38f150ba6a0c
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + SearchTerm + "&page=" + numResults + "&pub_year=" + year + "&api-key=mXyNr2iOtjbdhpk5dAR4WjUVoO6JQW2d"
 
 
 
-$.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function(response) {
-<<<<<<< HEAD
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            console.log(response);
 
-
-        }
-=======
-console.log(response);
-    
-    });
+        });
 });
->>>>>>> 46c20a52dfdfac8c2b65ec1c4a4b38f150ba6a0c
